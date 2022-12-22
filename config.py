c553e6cbfd6fcdabbfd195b620a871b8e938f938
DQ.BOT <96868296+Abcdviahnibshsjsakakab@users.noreply.github.com>
2022-12-22 20:56:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,19 +9,19 @@ if os.path.exists("local.env"):
 API_ID = int(getenv("API_ID", "6435225")) #optional
 API_HASH = getenv("API_HASH", "") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "-1001521670151").split()))
 OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC")
-ALIVE_TEXT = getenv("ALIVE_TEXT")
-PM_LOGGER = getenv("PM_LOGGER")
-LOG_GROUP = getenv("LOG_GROUP")
-GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://A:A@cluster0.aim6z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+BOT_TOKEN = getenv("BOT_TOKEN", "5047736431:AAGbF856jE9DwF7-xaUxP95EPYezRytH5tQ")
+ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/e0150d57b5a7d36e064a2.jpg")
+ALIVE_TEXT = getenv("ALIVE_TEXT", "IAM ALIVE Anu kunda..///")
+PM_LOGGER = getenv("PM_LOGGER", "")
+LOG_GROUP = getenv("LOG_GROUP", "")
+GIT_TOKEN = getenv("GIT_TOKEN", "") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "BQBiMZkAKrwzR-yPn8wTcusbu0UkeujhC6G45259rnICFwyuGsH2vLB9c0J1ddoc8BO8Vz-vppZ_CjpOlw0xnjmOg-j43bFP3dq4_jflYmNnScL3z-9UPYksK5SODRRtZ4FmOwRkEfDNC3PzDJLfdupUJwvQv1yT5G6BCAvgosAkr7tMyt7SVMpF6Ev_gfxWIFynsOJoD8EN1k2IDMlU6isKHQPzcvtjT_N3BaLTamquEypxRVxLLDwoizrRWOZLHdtQqMI5maaM0KvCTF5if5DJxpZ7p-B42ldSGbwbbaYBR4WZUbLludsfRW5lKtBXuBuRKgwSDtyl2n-DsDZDyKZVY2V-eAAAAABLAEvyAA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
